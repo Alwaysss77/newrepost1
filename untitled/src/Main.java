1720c4f5a19a79848c328714a606d39bd9b6b59b
Alwaysss77 <jasurbekpolatov1213@gmail.com>
2025-10-29 22:30:46 +0500

    some_change

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello word");
+        System.out.println("Nma gap garanglar");
+        System.out.println("Moooooollar");
 
     }
 }
